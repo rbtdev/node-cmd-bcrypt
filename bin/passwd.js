c8b6cfa0827494b681973bba1a923d7cca22243e
Robert Thuleen <rob.thuleen@fuseintegration.com>
2016-12-24 10:56:43 -0800

    Added plaintext and separator options

--- bin/passwd.js
+++ bin/passwd.js
@@ -3,12 +3,18 @@
 var bcrypt = require('bcryptjs')
 var options = require('commander')
 options
-    .version('1.0.0')
+    .version('0.0.3')
     .usage('[options]')
+    .description("Reads stdin and uses bcrypt to hash each line. Writes " +
+        "hash values to stdout one per line in the same order as the input lines.")
     .option('-r, --rounds <n>', "Complexity factor for salt generation [10]", parseInt, 10)
+    .option('-p, --plaintext', "Include plaintext at beginning of line, sepated by ' '")
+    .option('-s, --separator <separator>', "Use as a separator between plaintext and hash [':']")
     .parse(process.argv);
 
-var rounds = options.rounds;
+var rounds = options.rounds || 10;
+var plaintext = options.plaintext;
+var separator = options.separator || ":";
 var data = "";
 
 process.stdin.resume();
@@ -21,11 +27,14 @@ process.stdin.on('data', function (chunk) {
 process.stdin.on('end', function () {
     var lines = data.split('\n');
     lines.forEach(function (line) {
-        if (line.length > 0) process.stdout.write(hash(line));
+        if (line.length > 0) {
+            var out = (plaintext ? (line + separator) : "") + hash(line, rounds) + '\n';
+            process.stdout.write(out);
+        }
     })
 });
 
-function hash(passwd) {
+function hash(passwd, rounds) {
     var salt = bcrypt.genSaltSync(parseInt(rounds));
     try {
         return (bcrypt.hashSync(passwd, salt));
