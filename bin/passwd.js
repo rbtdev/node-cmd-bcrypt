4619ca981e777cf030ce86c0ec4512da1494cdf5
Robert Thuleen <rob.thuleen@fuseintegration.com>
2016-12-24 10:35:40 -0800

    Use stdin

--- bin/passwd.js
+++ bin/passwd.js
@@ -4,20 +4,34 @@ var bcrypt = require('bcryptjs')
 var options = require('commander')
 options
     .version('1.0.0')
-    .usage('[options] <password>')
+    .usage('[options]')
     .option('-r, --rounds <n>', "Complexity factor for salt generation [10]", parseInt, 10)
     .parse(process.argv);
 
-var passwd = options.args[0];
 var rounds = options.rounds;
-if (!passwd) return options.help();;
-var salt = bcrypt.genSaltSync(parseInt(rounds));
-try {
-    var hash = bcrypt.hashSync(passwd, salt);
-    process.stdout.write(hash);
-    process.exit(0);
-} catch (ex) {
-    console.log("Caught error");
-    process.stderr.write(JSON.stringify(ex));
-    process.exit(1);
+var data = "";
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', function (chunk) {
+    data += chunk;
+});
+
+process.stdin.on('end', function () {
+    var lines = data.split('\n');
+    lines.forEach(function (line) {
+        if (line.length > 0) process.stdout.write(hash(line));
+    })
+});
+
+function hash(passwd) {
+    var salt = bcrypt.genSaltSync(parseInt(rounds));
+    try {
+        return (bcrypt.hashSync(passwd, salt));
+    } catch (ex) {
+        console.log("Caught error");
+        process.stderr.write(JSON.stringify(ex));
+        process.exit(1);
+    }
 }
