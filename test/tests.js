4c1c17541d2c931a31f589eaf020e1f3a88be30a
Robert Thuleen <rob.thuleen@fuseintegration.com>
2016-12-24 17:40:51 -0800

    Added mocha test

--- test/tests.js
+++ test/tests.js
@@ -1,25 +1,35 @@
 var passwdjs = require('../bin/passwdjs');
+var bcrypt = require('bcryptjs');
+var expect = require('chai').expect;
 
-var lines = [
-    'passwd1',
-    'passwd2',
-    'passwd3',
-    'passwd4'
-];
+describe('hash array', function () {
+    it('should hash an array of plaintext', function () {
+        var lines = [
+            'passwd1',
+            'passwd2',
+            'passwd3',
+            'passwd4'
+        ];
+        var lineNumber = 0;
+        var opts = {
+            rounds: 10,
+            plaintext: false
+        }
+        passwdjs(lines, opts)
+            .on('line', gotLine)
+            .on('done', done);
 
-var opts = {
-    rounds: 10,
-    plaintext: true,
-    separator: "-"
-}
-passwdjs(lines, opts)
-    .on('line', gotLine)
-    .on('done', done);
+        function gotLine(line) {
+            console.log("Checking", lines[lineNumber]);
+            bcrypt.compare(lines[lineNumber], line, function (err, match) {
+                expect(err).to.equal(null);
+                expect(match).to.equal(true);
+            })
+            lineNumber++;
+        }
 
-function gotLine(line) {
-    console.log(line);
-}
-
-function done() {
-    console.log('done');
-}
+        function done(results) {
+            expect(results.length).to.equal(lines.length);
+        }
+    })
+})
