a443765b863250d85538ba43ab139bd9294317a4
Robert Thuleen <rob.thuleen@fuseintegration.com>
2016-12-26 10:14:53 -0800

    Updated mocha tests

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ var bcrypt = require('bcryptjs');
 var EventEmitter = require('events').EventEmitter;
 var async = require('async');
 
-function passwdjs(lines, options) {
-
+function passwdjs(passwd, options) {
     var e = new EventEmitter();
+    if (!passwd) return setImmediate(e.emit('error', new Error("Invalid passwd parameter")))
+    var lines = Array.isArray(passwd) ? passwd : [].push(passwd);
+    var opts = options ? options : {
+        rounds: 10,
+        plaintext: false
+    }
     async.mapSeries(lines, hash, function (err, results) {
         e.emit('done', results);
     })
 
     function hash(line, cb) {
-        bcrypt.genSalt(options.rounds, function (err, salt) {
+        bcrypt.genSalt(opts.rounds, function (err, salt) {
             if (err) return cb(err);
             if (line.length === 0) return cb();
             bcrypt.hash(line, salt, function (err, hashed) {
                 if (err) return cb(err);
-                var out = (options.plaintext ? (line + options.separator) : "") + hashed;
+                var out = (opts.plaintext ? (line + opts.separator) : "") + hashed;
                 e.emit('line', out);
                 return cb(null, out);
             });
--- test/test.js
+++ test/test.js
@@ -1,35 +1,71 @@
 var passwdjs = require('../');
 var bcrypt = require('bcryptjs');
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
 
-describe('hash array', function () {
-    it('should hash an array of plaintext', function () {
-        var lines = [
-            'passwd1',
-            'passwd2',
-            'passwd3',
-            'passwd4'
-        ];
-        var lineNumber = 0;
-        var opts = {
-            rounds: 10,
-            plaintext: false
-        }
-        passwdjs(lines, opts)
-            .on('line', gotLine)
-            .on('done', done);
+var lineEvent = false;
+var doneEvent = false;
+var hash = null;
+var hashLines = [];
+var hashArray = [];
+var lineCount = 0;
+var passwords = [];
 
-        function gotLine(line) {
-            console.log("Checking", lines[lineNumber]);
-            bcrypt.compare(lines[lineNumber], line, function (err, match) {
-                expect(err).to.equal(null);
-                expect(match).to.equal(true);
-            })
-            lineNumber++;
-        }
 
-        function done(results) {
-            expect(results.length).to.equal(lines.length);
-        }
+
+
+describe("hash array of passwords", function (done) {
+    beforeEach(function (done) {
+        reset();
+        run(done);
+    })
+
+    it("Should emit a 'line' event for each password", function () {
+        expect(hash).to.be.a.string;
+        expect(lineCount).to.equal(passwords.length);
+    });
+
+    it("Should emit a 'done' event with an array of hashes", function () {
+        expect(doneEvent).to.be.true;
+        expect(hashArray.length).to.equal(passwords.length);
+        expect(hashLines).to.deep.equal(hashArray);
     })
 })
+
+function run(done) {
+    passwdjs(passwords, opts)
+        .on('line', gotLine)
+        .on('done', gotDone);
+
+    function gotLine(line) {
+        hashLines.push(line);
+        lineCount++;
+    }
+
+    function gotDone(hashed) {
+        doneEvent = true;
+        hashArray = hashed;
+        done();
+    }
+}
+
+function reset() {
+    lineEvent = false;
+    doneEvent = false;
+    hash = null;
+    hashLines = [];
+    hashArray = [];
+    lineCount = 0;
+
+    passwords = [
+        'passwd1',
+        'passwd2',
+        'passwd3',
+        'passwd4'
+    ];
+    opts = {
+        rounds: 10,
+        plaintext: false
+    }
+
+}
