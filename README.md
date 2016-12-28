495cd9c1b0d3898e89aa76c414c8e4d41e8bce32
Robert Thuleen <rob.thuleen@fuseintegration.com>
2016-12-28 07:41:14 -0800

    added subcommand 'hash'

--- README.md
+++ README.md
@@ -11,32 +11,48 @@ output of other commands.  The function accepts an array of strings, and an opti
 ###Command Line Utility
 ```
 $ npm install -g @rbtdev/node-cmd-bcrypt
-$ cat password_file.txt | passwdjs -r 12 -p -s "-" > hashmap.txt
+
 $ passwdjs --help
+  Usage: passwdjs <cmd>
 
-Usage: passwdjs [options]
+  Commands:
 
-  Reads stdin and uses bcrypt to hash each line. Writes hash values to stdout one per line in the same order as the input lines.
+    hash [options] [passwords...]  Uses bcryptjs to hash each passoword provided or reads lines from stdin if no passwords are provided.
+
+  Command line tool to hash and compare bcrypt passwords.
 
   Options:
 
-    -h, --help                   output usage information
-    -V, --version                output the version number
-    -r, --rounds <n>             Complexity factor for salt generation [10]
-    -p, --plaintext              Include plaintext at beginning of line [false]
-    -s, --separator <separator>  Use as a separator between plaintext and hash [':']
+    -h, --help     output usage information
+    -V, --version  output the version number
 
   Examples:
 
-    $ cat file.txt | passwdjs -r 10 -p -s " "
-    hello $2a$10$3GV2hZDNBNgiU0.Zf.3t4ulEMk12FfDfhlBhE/sAFN/IkII5XNvSO
-    goodbye $2a$10$lwC3VIjrqa1BMpF1kGg5Du.H5eEXL/fY.SV0vNFFg3QKDx.yMPO0a
-    onemoretime $2a$10$ocrWkgLpZs51Hey2gspKLuo2OnXfj8hZB8YqyTt3ZF3O5DrNXWgJy
-    anotherone $2a$10$0RxjXB/kFwlA7Wk9VCjJLeZ9AbTSH9Son9Pct0Oex9rFkCyNlzai2
+    $ passwdjs hash -j -r 12 password1 password2 password3
+    {
+      "$2a$12$QHEdi2VK9HhipSs9rVfQYOS0FwKnjxoL/1mhqkD67lZJ0luPhWX1u": "password1",
+      "$2a$12$5pHzuObUEE5wSEOWBIayEe1Uv1RIRXpPVTxaKIXpW07t9fBrBJnWO": "password2",
+      "$2a$12$H5eV9qwx82rZeQ8PbYaKW.Unc8uiz2LrvgLmCStdY9EFIiC44lxem": "password3",
+    }
 
-    $ echo "password" | passwdjs passwdjs -r 15
-    $2a$15$JqPdC1T1H7KNu/p0w6dOL.fNdC2eiBDJnaZrE54HvHJCZgQjbbxmS
+    $ cat passwords.txt
+    password1
+    password2
+    password3
+    password4
+    password5
 
+    $ cat passwords.txt | passwdjs hash -r 12 -j
+    {
+      "$2a$12$ErsnLyXSXaupErd5imARB.S6sl6QD8m0a.Z0ECGA5KsqiEVJs80W2": "password1",
+      "$2a$12$6k/WRP17ba/XfewrzpOz5OJIIbiz12ocHbDJvRpk.USFTioUCnOem": "password2",
+      "$2a$12$hkelc1Xu.jFq3UpRmidpCOvZcUEWIpKks0ZewkmArjLSxnCwhoDJ2": "password3",
+      "$2a$12$WK1KVKmiH.heNTpUwR2IcuXyzCjrO64Nun/A5R11DavKcc48h0Epu": "password4",
+      "$2a$12$86N8t.Ha/RbhM.VYaFJI6uUbZI3g8f93A2pmWz7AAReZ8aAYQm2KO": "password5"
+    }
+
+    $ echo "password" | passwdjs hash -r 15
+    $2a$15$mT4C4CHQuTcumZG74JlGhen2e.b9yAWVtIFREq9Pge6dDXUkHiZPG
 ```
 ###As a required module
 
--- bin/passwdjs.js
+++ bin/passwdjs.js
@@ -6,41 +6,74 @@ var command = require('commander')
 
 command
     .version(version)
-    .usage('[options]')
-    .description(
-        "Reads stdin and uses bcrypt to hash each line.  Writes each hash" +
-        "value to stdout in the order of the original plaintext.  If the '-j' " +
-        "option is used, the hash values are placed into a JSON object with " +
-        "the original plaintext as the value of the hash.")
+    .usage('<cmd>')
+    .description("Command line tool to hash and compare bcrypt passwords.");
 
-.option('-r, --rounds <n>', "Complexity factor for salt generation [10]", parseInt, 10)
+command
+    .command('hash [passwords...]')
+    .description("Uses bcryptjs to hash each passoword provided or reads lines from stdin if no passwords are provided.")
+    .option('-r, --rounds <n>', "Complexity factor for salt generation [10]", parseInt, 10)
     .option('-j, --json', 'Output a JSON object which is a map of plaintext to hash')
+    .action(hash)
 
 command.on('--help', function () {
     console.log('  Examples:');
     console.log('');
-    console.log('    $ cat file.txt | passwdjs -r 10 -p -s " "');
-    console.log('    $ echo "password" | passwdjs -r 15');
+    console.log('    $ passwdjs hash -j -r 12 password1 password2 password3 ');
+    console.log('    {');
+    console.log('      "$2a$12$QHEdi2VK9HhipSs9rVfQYOS0FwKnjxoL/1mhqkD67lZJ0luPhWX1u": "password1",');
+    console.log('      "$2a$12$5pHzuObUEE5wSEOWBIayEe1Uv1RIRXpPVTxaKIXpW07t9fBrBJnWO": "password2",');
+    console.log('      "$2a$12$H5eV9qwx82rZeQ8PbYaKW.Unc8uiz2LrvgLmCStdY9EFIiC44lxem": "password3",');
+    console.log('    }');
+    console.log('');
+    console.log('    $ cat passwords.txt');
+    console.log('    password1');
+    console.log('    password2');
+    console.log('    password3');
+    console.log('    password4');
+    console.log('    password5');
+    console.log('');
+    console.log('    $ cat passwords.txt | passwdjs hash -r 12 -j');
+    console.log('    {');
+    console.log('      "$2a$12$ErsnLyXSXaupErd5imARB.S6sl6QD8m0a.Z0ECGA5KsqiEVJs80W2": "password1",');
+    console.log('      "$2a$12$6k/WRP17ba/XfewrzpOz5OJIIbiz12ocHbDJvRpk.USFTioUCnOem": "password2",');
+    console.log('      "$2a$12$hkelc1Xu.jFq3UpRmidpCOvZcUEWIpKks0ZewkmArjLSxnCwhoDJ2": "password3",');
+    console.log('      "$2a$12$WK1KVKmiH.heNTpUwR2IcuXyzCjrO64Nun/A5R11DavKcc48h0Epu": "password4",');
+    console.log('      "$2a$12$86N8t.Ha/RbhM.VYaFJI6uUbZI3g8f93A2pmWz7AAReZ8aAYQm2KO": "password5"');
+    console.log('    }');
+    console.log('');
+    console.log('    $ echo "password" | passwdjs hash -r 15');
+    console.log('    $2a$15$mT4C4CHQuTcumZG74JlGhen2e.b9yAWVtIFREq9Pge6dDXUkHiZPG');
     console.log('');
 });
-
 command.parse(process.argv);
 
-var options = {
-    rounds: parseInt(command.rounds) || 10,
-    json: command.json || false
+function hash(passwords, opts) {
+    var options = {
+        rounds: opts.rounds || 10,
+        json: opts.json || false
+    }
+
+    if (passwords.length === 0) {
+        readStdIn(hashData, options);
+    } else {
+        hashData(passwords, options);
+    }
 }
 
-var data = "";
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', function (chunk) {
-    data += chunk;
-});
-process.stdin.on('end', hashData);
+function readStdIn(cb, options) {
+    var data = "";
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function (chunk) {
+        data += chunk;
+    });
+    process.stdin.on('end', function () {
+        cb(data.split('\n'), options);
+    });
+}
 
-function hashData() {
-    var lines = data.split('\n');
+function hashData(lines, options) {
     passwdjs(lines, options)
         .on('line', function (hashed) {
             if (!options.json) process.stdout.write(hashed + '\n');
